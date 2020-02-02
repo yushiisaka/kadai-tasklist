4f18a04aa264918014ebd4198100ae967bfc464e
I.Y <iyushi1010@gmail.com>
2020-02-02 08:00:45 +0000

    new colom add

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -41,7 +41,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+         $this->validate($request, [
+            'status' => 'required|max:10',   
+            'content' => 'required|max:181',
+        ]);
         $task = new Task;
+         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -85,7 +90,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',   
+            'content' => 'required|max:181',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
